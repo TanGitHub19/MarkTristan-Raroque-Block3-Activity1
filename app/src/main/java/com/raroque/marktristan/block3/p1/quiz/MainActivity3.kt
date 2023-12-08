fad7aaeee5099fdbe886274c11125b3a4e1116a7
Mark Tristan Raroque <marktristanraroque@gmail.com>
2023-12-08 22:46:14 +0800

    Added Feature: Result Display

--- app/src/main/java/com/raroque/marktristan/block3/p1/quiz/MainActivity3.kt
+++ app/src/main/java/com/raroque/marktristan/block3/p1/quiz/MainActivity3.kt
@@ -2,10 +2,19 @@ package com.raroque.marktristan.block3.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var scoreTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        scoreTextView = findViewById(R.id.score)
+
+        val score = intent.getIntExtra("score", 0)
+        scoreTextView.text = "$score"
     }
 }
