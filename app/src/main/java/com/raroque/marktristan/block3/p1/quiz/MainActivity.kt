d3b36839421c33f41df4e6bf42aca2ec6fc587f5
Mark Tristan Raroque <marktristanraroque@gmail.com>
2023-12-08 23:21:17 +0800

    Added Feature: Exit Button Try Again Button Fix some UI

--- app/src/main/java/com/raroque/marktristan/block3/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/raroque/marktristan/block3/p1/quiz/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.raroque.marktristan.block3.p1.quiz
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var startButton: Button
+    private lateinit var exitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startButton = findViewById(R.id.start_button)
+        exitButton = findViewById(R.id.Exit_button)
 
-        startButton.setOnClickListener{
+        startButton.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
+        }
 
+        exitButton.setOnClickListener {
+            finishAffinity()
         }
     }
 }
--- app/src/main/java/com/raroque/marktristan/block3/p1/quiz/MainActivity3.kt
+++ app/src/main/java/com/raroque/marktristan/block3/p1/quiz/MainActivity3.kt
@@ -1,12 +1,15 @@
 package com.raroque.marktristan.block3.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
 
     private lateinit var scoreTextView: TextView
+    private lateinit var retry: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,12 @@ class MainActivity3 : AppCompatActivity() {
 
         val score = intent.getIntExtra("score", 0)
         scoreTextView.text = "$score"
+
+        retry = findViewById(R.id.try_Again)
+
+        retry.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
